4c8cf287f5db4da4f74b2ec260025e372c93c6be
artvertex <4tharraofdagon@gmail.com>
2018-05-01 01:48:33 +0300

    add function for decoding

--- cipher-of-caesar/cipher.js
+++ cipher-of-caesar/cipher.js
@@ -1,5 +1,6 @@
 (function() {
     const intext = document.getElementById('in');
+    const outtext = document.getElementById('intwo');
     intext.oninput = function() {
         const offset = 3;
         let str = this.value;
@@ -10,5 +11,17 @@
             out += String.fromCharCode(code);
         }
         document.getElementById('out').innerHTML = out;
+    };
+
+    outtext.oninput = function() {
+        const offset = 3;
+        let str = this.value;
+        let out = '';
+        for (let i = 0; i < str.length; i++) {
+            let code = str.charCodeAt(i);
+            code = code - offset;
+            out += String.fromCharCode(code);
+        }
+        document.getElementById('outtwo').innerHTML = out;
     };
 })();
